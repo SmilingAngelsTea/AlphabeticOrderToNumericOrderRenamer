b58de8292e762f692010684bd921243594b3a36f
SmilingAngelsTea <59475333+SmilingAngelsTea@users.noreply.github.com>
2020-03-15 21:41:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabeticOrderToNumericOrderRenamer {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("AlphabeticOrderToNumericOrderRenamer\n");
+            string path = "";
+            if (args.Length < 1) {
+                Console.WriteLine("No location specified. Using the current working directory.");
+                path = Directory.GetCurrentDirectory();
+            } else {
+                path = args[0];
+            }
+            Console.WriteLine("Path: " + path);
+            string[] files = Directory.GetFiles(path).OrderBy(f => f).ToArray();
+            for(int i = 0; i < files.Length; i++) {
+                string number = "";
+                if(i < 10) {
+                    number = "0" + i.ToString();
+                } else {
+                    number = i.ToString();
+                }
+                string outer = Path.GetFileName(files[i]) + " to " + number + Path.GetExtension(files[i]);
+                try {
+                    File.Move(files[i], path + "\\" + number + Path.GetExtension(files[i]));
+                } catch {
+                    outer = outer + " failed.";
+                    Console.WriteLine(outer);
+                }
+                outer = outer + " succeeded.";
+                Console.WriteLine(outer);
+            }
+        }
+    }
+}
